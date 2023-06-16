32d3ac51ae076af76c12f1d2ee5f43881e4fd53c
Documenter.jl <documenter@juliadocs.github.io>
2023-06-16 02:28:45 +0000

    build based on b4ba3c9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LagrangianPerturbationTheory","category":"page"},{"location":"#LagrangianPerturbationTheory","page":"Home","title":"LagrangianPerturbationTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LagrangianPerturbationTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LagrangianPerturbationTheory]","category":"page"}]
+}
