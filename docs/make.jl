471e6827bb4390f5c58bfcaf198506b53c8cc13d
Zack Li <xzackli@gmail.com>
2023-07-01 19:39:24 -0400

    clean up code, add filename rounding

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,7 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
-        "Background Cosmology" => "background_cosmology.md",
+        "Cosmology" => "cosmology.md",
         "API" => "api.md"
     ],
 )
