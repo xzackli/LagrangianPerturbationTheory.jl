fb3d3624cee22fef6ed0ce1389e995f683bc59a2
xzackli <xzackli@gmail.com>
2024-05-22 16:19:05 -0700

    slurm jobs for FFT filtering a big box on NERSC

--- run/plot_filtered.jl
+++ run/plot_filtered.jl
@@ -1,8 +1,8 @@
 
 using Makie, CairoMakie, HDF5
 
-fname_orig = "/fs/lustre/project/act/zack/ICs/Fvec_7700Mpc_n6144_nb30_nt16_no768_p.h5"
-fname_filt = "filtered_output.h5"
+fname_orig = "/pscratch/sd/x/xzackli/websky_convert/data/Fvec_7700Mpc_n6144_nb30_nt16_no768_p.h5"
+fname_filt = "run/filtered_output.h5"
 
 plot_slice = (1:100, 1:100, 1)
 m_original = h5read(fname_orig, "data", plot_slice)
