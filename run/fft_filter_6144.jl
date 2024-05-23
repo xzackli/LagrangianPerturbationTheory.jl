909b6791d4ad0dac91ef8659c6f4d5782101f193
xzackli <xzackli@gmail.com>
2024-05-23 05:13:52 -0700

    cleaned up FFT filter further

--- run/fft_filter_6144.jl
+++ run/fft_filter_6144.jl
@@ -14,29 +14,19 @@ fname = "/pscratch/sd/x/xzackli/websky_convert/displacementprocessing/Fvec_7700M
 
 pen = Pencil(dims, comm)
 transform = Transforms.RFFT()
-plan = PencilFFTPlan(pen, transform)
-u = allocate_input(plan)
+plan = PencilFFTPlan(pen, transform, Float32)
 
-# function setup_grid!(fname, u, comm)
-#     global_u = global_view(u)
-#     global_axes = map( ax -> first(ax):last(ax), axes(global_u) )
-#     u .= h5read(fname, "data", global_axes)
-#     println("read $(MPI.Comm_rank(comm)): $(global_axes)")
-# end
-
-
-# setup_grid!(fname, u, comm)
-
-open(PencilArrays.PencilIO.PHDF5Driver(), fname, comm, read=true) do ff
-    read!(ff, u, "data")
-end
-println("FINISHED READING, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
-
-
-function filter_grid(u, plan, dims)
-
+function setup_grid(fname, plan, comm)
+    u = allocate_input(plan)
     v = allocate_output(plan)
+    open(PencilArrays.PencilIO.PHDF5Driver(), fname, comm, read=true) do ff
+        read!(ff, u, "data")
+    end
+    println("FINISHED READING, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
+    return u, v
+end
 
+function filter_grid!(u::AbstractArray{T}, v, plan, dims) where T
     mul!(v, plan, u)
 
     kx = fftfreq(dims[1])
@@ -47,15 +37,16 @@ function filter_grid(u, plan, dims)
     grid_fourier = localgrid(v, kvec)
 
     σ = 2.5
-    filter_factor = -2 * π^2 * σ^2
+    filter_factor = T(-2 * π^2 * σ^2)
     @. v *= exp( filter_factor * (grid_fourier[1]^2 + grid_fourier[2]^2 +  grid_fourier[3]^2)  )
 
     ldiv!(u, plan, v)  # now w ≈ u
     return u
 end
 
-
-u = filter_grid(u, plan, dims)
+u, v = setup_grid(fname, plan, comm)
+Base.GC.gc()
+filter_grid!(u, v, plan, dims)
 println("FINISHED FILTERING, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
 
 open(PencilArrays.PencilIO.PHDF5Driver(), "filtered_$(fname)", MPI.COMM_WORLD; write=true) do ff
--- run/test_fft_no768.jl
+++ run/test_fft_no768.jl
@@ -14,30 +14,23 @@ comm = MPI.COMM_WORLD
 fname = "/pscratch/sd/x/xzackli/websky_convert/data/Fvec_7700Mpc_n6144_nb30_nt16_no768_p.h5"
 
 
+pen = Pencil(dims, comm)
+transform = Transforms.RFFT()
+plan = PencilFFTPlan(pen, transform, Float32)
 
-function filter_grid(fname, dims, comm)
-    pen = Pencil(dims, comm)
-    transform = Transforms.RFFT()
-    # Create plan
-    plan = PencilFFTPlan(pen, transform)
-
-    # In our example, this returns a 3D PencilArray of real data (Float64).
+function setup_grid(fname, plan, comm)
     u = allocate_input(plan)
+    open(PencilArrays.PencilIO.PHDF5Driver(), fname, comm, read=true) do ff
+        read!(ff, u, "data")
+    end
+    println("FINISHED READING, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
+    return u
+end
 
-    # read in the HDF5 handle
-    # read local slice
-    # set the local pencil to it
+function filter_grid!(u::AbstractArray{T}, plan, dims) where T
 
-    global_u = global_view(u)
-    global_axes = map( ax -> first(ax):last(ax), axes(global_u) )
-    u .= h5read(fname, "data", global_axes)
-
-    println("$(MPI.Comm_rank(comm)): $(global_axes)")
-
-    # In our example, this returns a 3D PencilArray of complex data (Complex{Float64}).
     v = allocate_output(plan)
 
-    # Apply plan on `u` with `v` as an output
     mul!(v, plan, u)
 
     kx = fftfreq(dims[1])
@@ -48,16 +41,17 @@ function filter_grid(fname, dims, comm)
     grid_fourier = localgrid(v, kvec)
 
     σ = 2.5
-    filter_factor = -2 * π^2 * σ^2
+    filter_factor = T(-2 * π^2 * σ^2)
     @. v *= exp( filter_factor * (grid_fourier[1]^2 + grid_fourier[2]^2 +  grid_fourier[3]^2)  )
 
     ldiv!(u, plan, v)  # now w ≈ u
     return u
 end
 
-u = filter_grid(fname, dims, comm)
-
-println("WRITING, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
+u = setup_grid(fname, plan, comm)
+Base.GC.gc()
+filter_grid!(u, plan, dims)
+println("FINISHED FILTERING, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
 
 open(PencilArrays.PencilIO.PHDF5Driver(), "filtered_output.h5", MPI.COMM_WORLD; write=true) do ff
     ff["data"] = u
